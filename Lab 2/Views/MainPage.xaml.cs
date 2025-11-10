41830377d9ef6d01a2c321b013b337d3321e99c0
reagan1133 <reagantong@hotmail.com>
2025-11-10 16:46:28 +0800

    1

---
+++ Lab 2/Views/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace Lab_2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnFavoriteClicked(object sender, EventArgs e)
+        {
+            
+            DisplayAlert("Favorite", "You have added this pet to your favorites!", "OK");
+        }
+    }
+}
